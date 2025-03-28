74930d7d80fef634362b4fd6062751558ef4c156
John Strickler <jstrickler@gmail.com>
2025-03-28 15:50:14 +0000

    end of class

--- EXAMPLES/flask_examples/flask_hello.py
+++ EXAMPLES/flask_examples/flask_hello.py
@@ -2,7 +2,6 @@ from flask import Flask
 
 app = Flask(__name__)  # create Flask application object
 
-
 @app.route('/')  # assign this view function to '/' (i.e., the "root: of the web site)
 def index():  # define the view function
     return '<h1>Hello, Flask world!</h1>' # return HTML for the page to display
